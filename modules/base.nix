a9d57694b91f10ef91da2524edb982ccb8a36df2
phaer <hello@phaer.org>
2022-06-14 22:00:10 +0200

    base: re-structure

--- modules/base.nix
+++ modules/base.nix
@@ -1,4 +1,5 @@
 { config, lib, pkgs, modulesPath, ... }:
+with lib;
 {
   imports = [
     "${modulesPath}/profiles/minimal.nix"
@@ -6,85 +7,114 @@
 
   # Disable core stuff which does not have a "enable" flag.
   disabledModules = [
-      "system/boot/systemd/logind.nix"
-      "system/boot/systemd/coredump.nix"
+    "system/boot/systemd/logind.nix"
+    "system/boot/systemd/coredump.nix"
   ];
 
-  config = {
-    nix.extraOptions = "extra-experimental-features = nix-command flakes";
+  config =
+    let
+      # default is 100, mkForce is 50
+      mkOverride' = mkOverride 60;
+    in
+      lib.mkMerge [
+        {
+          nix.extraOptions = "extra-experimental-features = nix-command flakes";
 
-    systemd.package = lib.mkForce pkgs.systemdMinimal;
-    boot.initrd.systemd = {
-      enable = true;
-      emergencyAccess = true;
-    };
-    boot.loader.grub.enable = false;
+          systemd.package = mkOverride' pkgs.systemdMinimal;
+          boot.initrd.systemd = {
+            enable = true;
+            emergencyAccess = true;
+          };
+          boot.loader.grub.enable = mkOverride' false;
+          environment.noXlibs = mkOverride' true;
+        }
 
-    ## Disable Services
+        {
+          ## Disable Services
+          boot.enableContainers = mkOverride' false;
+          hardware.enableRedistributableFirmware = mkOverride' false;
+          security.polkit.enable = mkOverride' false;
+          services.dbus.enable = mkOverride' false;
+          services.logrotate.enable = mkOverride' false;
+          services.timesyncd.enable = mkOverride' false;
+          services.udisks2.enable = mkOverride' false;
+          networking.firewall.enable = mkOverride' false;
+          services.nscd.enable = mkOverride' false;
+        }
 
-    boot.enableContainers = lib.mkForce false;
-    environment.noXlibs = true;
-    security.polkit.enable = lib.mkForce false;
-    hardware.enableRedistributableFirmware = lib.mkForce false;
-    services.dbus.enable = lib.mkForce false;
-    services.udisks2.enable = false;
-    services.timesyncd.enable = lib.mkForce false;
-    services.nscd.enable = lib.mkForce false;
-    system.nssModules = lib.mkForce [];
+        (mkIf (!config.services.nscd.enable) {
+          system.nssModules = mkForce [];
+        })
 
-    ## Configure kernel and init ramdisk
-    boot.initrd.kernelModules = [ "squashfs" "loop" "overlay" ];
-    boot.supportedFilesystems = lib.mkForce config.boot.initrd.supportedFilesystems;
-    boot.kernelParams = [ "systemd.log_level=info" "systemd.log_target=console" "systemd.journald.forward_to_console=1" ];
+        {
+          ## Configure kernel and init ramdisk
+          boot.initrd.kernelModules = [ "squashfs" "loop" "overlay" ];
+          boot.supportedFilesystems = mkOverride' config.boot.initrd.supportedFilesystems;
+          boot.kernelParams = [ "systemd.log_level=info" "systemd.log_target=console" "systemd.journald.forward_to_console=1" ];
 
-    # boot.initrd.systemd does not use boot.post*Commands, and so we need to support creating directories
-    # for the overlay nix store ourselves.
-    boot.initrd.systemd = {
-      mounts = [{
-        where = "/sysroot/nix/store";
-        what = "overlay";
-        type = "overlay";
-        options = "lowerdir=/sysroot/nix/.ro-store,upperdir=/sysroot/nix/.rw-store/store,workdir=/sysroot/nix/.rw-store/work";
-        wantedBy = ["local-fs.target"];
-        before = ["local-fs.target"];
-        requires = ["sysroot-nix-.ro\\x2dstore.mount" "sysroot-nix-.rw\\x2dstore.mount" "rw-store.service"];
-        after = ["sysroot-nix-.ro\\x2dstore.mount" "sysroot-nix-.rw\\x2dstore.mount" "rw-store.service"];
-        unitConfig.IgnoreOnIsolate = true;
-      }];
-      services.rw-store = {
-        after = ["sysroot-nix-.rw\\x2dstore.mount"];
-        unitConfig.DefaultDependencies = false;
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "/bin/mkdir -p 0755 /sysroot/nix/.rw-store/store /sysroot/nix/.rw-store/work /sysroot/nix/store";
-        };
-      };
-    };
-    systemd.tmpfiles.rules = [
-      "f /etc/NIXOS 0644 root root -"
-      "d /boot 0644 root root -"
-    ];
+          # boot.initrd.systemd does not use boot.post*Commands, and so we need to support creating directories
+          # for the overlay nix store ourselves.
+          #
+          #boot.postBootCommands =
+          #  ''
+          #    # After booting, register the contents of the Nix store
+          #    # in the Nix database in the tmpfs.
+          #    ${config.nix.package}/bin/nix-store --load-db < /nix/store/nix-path-registration
 
-    ## Configure filesystems
-    fileSystems."/" =
-      {
-        fsType = "tmpfs";
-        options = [ "mode=0755" ];
-        neededForBoot = true;
-      };
+          #    # nixos-rebuild also requires a "system" profile and an
+          #    # /etc/NIXOS tag.
+          #    touch /etc/NIXOS
+          #    ${config.nix.package}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+          #  '';
 
-    fileSystems."/nix/.ro-store" =
-      {
-        fsType = "squashfs";
-        device = "../nix-store.squashfs";
-        options = [ "loop" ];
-        neededForBoot = true;
-      };
-    fileSystems."/nix/.rw-store" =
-      {
-        fsType = "tmpfs";
-        options = [ "mode=0755" ];
-        neededForBoot = true;
-      };
-  };
+          systemd.tmpfiles.rules = [
+            "f /etc/NIXOS 0644 root root -"
+            "d /boot 0644 root root -"
+          ];
+          boot.initrd.systemd = {
+            mounts = [{
+              where = "/sysroot/nix/store";
+              what = "overlay";
+              type = "overlay";
+              options = "lowerdir=/sysroot/nix/.ro-store,upperdir=/sysroot/nix/.rw-store/store,workdir=/sysroot/nix/.rw-store/work";
+              wantedBy = ["local-fs.target"];
+              before = ["local-fs.target"];
+              requires = ["sysroot-nix-.ro\\x2dstore.mount" "sysroot-nix-.rw\\x2dstore.mount" "rw-store.service"];
+              after = ["sysroot-nix-.ro\\x2dstore.mount" "sysroot-nix-.rw\\x2dstore.mount" "rw-store.service"];
+              unitConfig.IgnoreOnIsolate = true;
+            }];
+            services.rw-store = {
+              after = ["sysroot-nix-.rw\\x2dstore.mount"];
+              unitConfig.DefaultDependencies = false;
+              serviceConfig = {
+                Type = "oneshot";
+                ExecStart = "/bin/mkdir -p 0755 /sysroot/nix/.rw-store/store /sysroot/nix/.rw-store/work /sysroot/nix/store";
+              };
+            };
+          };
+        }
+
+        {
+          ## Configure filesystems
+          fileSystems."/" =
+            {
+              fsType = "tmpfs";
+              options = [ "mode=0755" ];
+              neededForBoot = true;
+            };
+
+          fileSystems."/nix/.ro-store" =
+            {
+              fsType = "squashfs";
+              device = "../nix-store.squashfs";
+              options = [ "loop" ];
+              neededForBoot = true;
+            };
+          fileSystems."/nix/.rw-store" =
+            {
+              fsType = "tmpfs";
+              options = [ "mode=0755" ];
+              neededForBoot = true;
+            };
+        }];
 }
