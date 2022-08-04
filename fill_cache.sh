fefb9aa3253f9c7436cadc3f6a643d729f06a2df
phaer <hello@phaer.org>
2022-08-04 16:13:03 +0200

    add fill_cache.sh

---
+++ fill_cache.sh
@@ -0,0 +1,8 @@
+export CACHIX_CACHE="nix-dabei"
+export CACHIX_AUTH_TOKEN="$(gopass show -o cachix-nix-dabei)"
+nix flake archive --json \
+  | jq -r '.path,(.inputs|to_entries[].value.path)' \
+  | nix run nixpkgs#cachix push $CACHIX_CACHE
+nix build .#dist .#toplevel --json \
+  | jq -r '.[].outputs | to_entries[].value' \
+  | nix run nixpkgs#cachix push $CACHIX_CACHE
