7c2ea3b9614b5d4fb8d83a1e29e7ae506ce2f375
phaer <hello@phaer.org>
2022-08-06 21:38:59 +0200

    wip: draft hetzner auto-installer

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       imports = [
       ];
       systems = [ "x86_64-linux" "aarch64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: rec {
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
@@ -21,6 +21,72 @@
             inherit system;
             inherit (self.nixosConfigurations.default) config;
           };
+
+          hetznerInstaller = pkgs.linkFarmFromDrvs "hetzner-installer" [
+            self'.packages.zfsImage
+            (pkgs.writeShellScript "create-nixos-machine.sh" ''
+                set -euxo pipefail
+
+                SSH_ARGS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+                wait_for_ssh() {
+                    until ssh -o ConnectTimeout=2 $SSH_ARGS root@"$1" "true"
+                        do sleep 1
+                    done
+                }
+
+                # TODO remove or prompt
+                hcloud server delete installer-test \
+                    || true
+
+                hcloud server create \
+                    --start-after-create=false \
+                    --name installer-test \
+                    --type cx11 \
+                    --image debian-11 \
+                    --location nbg1 \
+                    --ssh-key "ssh key"
+
+                hcloud server enable-rescue \
+                    --ssh-key "ssh key" \
+                    installer-test
+
+                hcloud server poweron installer-test
+
+                export TARGET_SERVER=$(hcloud server ip installer-test)
+                wait_for_ssh "$TARGET_SERVER"
+                test "$(ssh $SSH_ARGS root@$TARGET_SERVER hostname)" = "rescue" \
+                    || exit 1
+
+                echo "Copying to $TARGET_SERVER"
+                rsync \
+                    -e "ssh $SSH_ARGS" \
+                    -Lvz \
+                    --info=progress2 \
+                    "./result/nixos-disk-image/nixos.root.qcow2" \
+                    "./result/wipe-disk-and-install.sh" \
+                    root@$TARGET_SERVER:
+
+                echo "Installing to $TARGET_SERVER"
+                echo "ssh $SSH_ARGS root@$TARGET_SERVER -t 'bash ./wipe-disk-and-install.sh'"
+            '')
+            (pkgs.writeScript "wipe-disk-and-install.sh" ''
+                #!/usr/bin/env bash
+                set -euxo pipefail
+
+                test "$(hostname)" = "rescue" || exit 1
+                TARGET_DISK="/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0-0-0-0"
+
+                # you could just use dd if you build the image with format=raw, but
+                # compressed qcow2 with lots of empty space in the image means ~700MB vs 3GB.
+                qemu-img convert -f qcow2 -O raw -p nixos.root.qcow2 "$TARGET_DISK"
+
+                # "create a new partition table" while using --append is effectively a no-op,
+                # but it adjusts the disk size in our GPT header, so that auto-expansion can work later on
+                echo 'label: gpt' | sfdisk --append "$TARGET_DISK"
+
+                reboot
+            '')
+          ];
         };
 
         devShells = {
@@ -33,7 +99,7 @@
             export HCLOUD_TOKEN="$(gopass show -o fancy.systems/external/hetzner.cloud)"
           '';
           };
-          default = devShells.deployment;
+          default = self'.devShells.deployment;
         };
 
       };
