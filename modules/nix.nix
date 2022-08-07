5bf919e5d697885395af01c61177d4e28f2611c4
phaer <hello@phaer.org>
2022-08-08 00:06:35 +0200

    nix: clone nix-dabei from github, don't pin nix

--- modules/nix.nix
+++ modules/nix.nix
@@ -6,8 +6,12 @@
   nix = {
     nixPath = ["nixpkgs=${inputs.nixpkgs}"];
     registry.nixpkgs.flake = inputs.nixpkgs;
-    registry.config.flake = inputs.self;
-    package = pkgs.nix;
+    registry.nix-dabei.to  = {
+      type = "github";
+      owner = "dep-sys";
+      repo = "nix-dabei";
+      ref = "zfs-disk-image";
+    };
     extraOptions = "experimental-features = nix-command flakes";
   };
 }
