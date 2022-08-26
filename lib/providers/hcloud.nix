a1e00e5e10222b3c5afe2127201b427e250ed71c
phaer <hello@phaer.org>
2022-08-26 16:27:48 +0200

    providers/hcloud: reload partition table

--- lib/providers/hcloud.nix
+++ lib/providers/hcloud.nix
@@ -83,6 +83,7 @@ rec {
       # but it adjusts the disk size in our GPT header, so that auto-expansion can work later on
       echo 'label: gpt' | sfdisk --append "$TARGET_DISK"
 
+      partx -u "$TARGET_DISK"
       mount "$TARGET_DISK-part2" /mnt
 
       python3 <<EOF
