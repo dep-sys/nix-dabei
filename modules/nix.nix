28d6ccf5cafe1f55b336bffce0d3ed9e235d8937
phaer <hello@phaer.org>
2022-10-06 17:23:39 +0200

    nix: remove nix-dabei from registry, only...
    set config if its url is available from instance-data, no
    default

--- modules/nix.nix
+++ modules/nix.nix
@@ -4,23 +4,16 @@
   config,
   inputs,
   ...
-}: let
-  nixDabeiRepo = {
-    type = "github";
-    owner = "dep-sys";
-    repo = "nix-dabei";
-    ref = "main";
-  };
-in {
+}: {
   nix = {
     nixPath = ["nixpkgs=${inputs.nixpkgs}"];
     registry = {
       nixpkgs.flake = inputs.nixpkgs;
-      nix-dabei.to = nixDabeiRepo;
-      config.to = if config.x.instance-data ? flake-url
-                  then config.x.instance-data.flake-url
-                  else nixDabeiRepo;
-    };
+    }
+    # config is only available after the first rebuild
+    // (if config.x.instance-data ? flake-url
+        then { config.to = config.x.instance-data.flake-url; }
+        else {});
     extraOptions = "experimental-features = nix-command flakes";
   };
 }
