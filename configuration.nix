06ac04fe34f04493394d9e449c67747f16c6ff34
phaer <hello@phaer.org>
2022-11-02 19:27:11 +0100

    use mkDefault in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -8,14 +8,14 @@
     nix-dabei = {
       zfs.enable = true;
       # disk to NUKE and DELETE ALL DATA. /dev/vda in qemu.
-      diskDevice = "/dev/vda";
+      diskDevice = lib.mkDefault "/dev/vda";
     };
     # TODO: replace this key!
     # It's my personal public key, provided as an example.
     # Nix 2.9 will allow us to use https://github.com/phaer.keys
     # as a flake input instead, but it's not in nixos-stable yet,
     # so we'll continue to support 2.8.
-    users.users.root.openssh.authorizedKeys.keys = [
+    users.users.root.openssh.authorizedKeys.keys = lib.mkDefault [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLopgIL2JS/XtosC8K+qQ1ZwkOe1gFi8w2i1cd13UehWwkxeguU6r26VpcGn8gfh6lVbxf22Z9T2Le8loYAhxANaPghvAOqYQH/PJPRztdimhkj2h7SNjP1/cuwlQYuxr/zEy43j0kK0flieKWirzQwH4kNXWrscHgerHOMVuQtTJ4Ryq4GIIxSg17VVTA89tcywGCL+3Nk4URe5x92fb8T2ZEk8T9p1eSUL+E72m7W7vjExpx1PLHgfSUYIkSGBr8bSWf3O1PW6EuOgwBGidOME4Y7xNgWxSB/vgyHx3/3q5ThH0b8Gb3qsWdN22ZILRAeui2VhtdUZeuf2JYYh8L"
     ];
   };
