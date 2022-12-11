9dc7582ca237f6c506b43ae96b22ea66034f8467
phaer <hello@phaer.org>
2022-12-12 00:03:26 +0100

    demo: move boot logic to boot.nix

--- demo/flake.nix
+++ demo/flake.nix
@@ -30,6 +30,7 @@
       web-01 = { name, nodes, pkgs, lib, config, modulesPath, ... }: {
         imports = [
           "${modulesPath}/profiles/qemu-guest.nix"
+          ./boot.nix
         ];
 
         config = {
@@ -38,38 +39,6 @@
             targetPort = 22;
             targetUser = "root";
             buildOnTarget = true;
-          };
-
-          boot.loader.grub = {
-            enable = true;
-            # We use "nodev" here to avoid having to declare the disk device (e.g. /dev/sda) in
-            # this config, so we'll only need to know about it during install-time.
-            device = "nodev";
-          };
-          # boot.loader.grub.device = "nodev" excludes grub from the system,
-          # but we need it inside the systems close and optimally inside PATH
-          # do be able to call it during auto-install.
-          environment.systemPackages = [ pkgs.grub2 ];
-
-          fileSystems = {
-            "/boot" =
-              {
-                device = "/dev/disk/by-partlabel/ESP";
-                fsType = "vfat";
-                neededForBoot = true;
-              };
-            "/" =
-              {
-                device = "rpool/local/root";
-                fsType = "zfs";
-                neededForBoot = true;
-              };
-            "/nix" =
-              {
-                device = "rpool/local/nix";
-                fsType = "zfs";
-                neededForBoot = true;
-              };
           };
 
           services.openssh = {
