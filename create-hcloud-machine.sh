37aa28bba2b34232c1ddbfbf4f5fe23cc1078868
phaer <hello@phaer.org>
2022-12-18 19:53:01 +0100

    deploy: add static ip example, move key to end

--- create-hcloud-machine.sh
+++ create-hcloud-machine.sh
@@ -30,7 +30,7 @@ rsync \
 
 ssh_authorized_key="$(base64 -w0 < ~/.ssh/yubikey.pub)"
 flake_url="github:dep-sys/nix-dabei/disko-runtime?dir=demo#nixosConfigurations.web-01"
-ssh $SSH_ARGS "root@$TARGET_SERVER" "./kexec-boot ssh_authorized_key=$ssh_authorized_key flake_url=$flake_url disks=zfs-single:/dev/sda"
+ssh $SSH_ARGS "root@$TARGET_SERVER" "./kexec-boot flake_url=$flake_url disks=zfs-single:/dev/sda ssh_authorized_key=$ssh_authorized_key"
 sleep 3
 wait_for_ssh "$TARGET_SERVER"
 
